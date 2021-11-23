206bb482c2534b272380cf8396de07230c0b1202
Richardson <richardm@ucl.br>
2021-11-23 19:08:22 -0300

    Alterando o mic e melhorando a pesquisa

--- EspelhoAssistente.py
+++ EspelhoAssistente.py
@@ -91,12 +91,12 @@ def ultimas_noticias():
 
 
 def pesquisar(trigger):
+    pesquisa = ''
     try:
-        pessoa = ''
         if 'pesquisa' in trigger:
-            pessoa = trigger.replace(' espelho meu pesquisa ', '')
+            pesquisa = trigger.replace('espelho espelho meu pesquisa ', '')
         wikipedia.set_lang("pt")
-        info = wikipedia.summary(pessoa, 1)
+        info = wikipedia.summary(pesquisa, 1)
         print(info)
         cria_audio(info)
     except:
@@ -109,6 +109,36 @@ responde('bem_vindo')
 
 
 ##### MICROFONE #####
+def monitora_audio():
+    microfone = sr.Recognizer()
+    with sr.Microphone() as source:
+        while True:
+            print("Aguardando o Comando: ")
+            audio = microfone.listen(source)
+            try:
+                trigger = microfone.recognize_google(audio, language='pt-br')
+                trigger = trigger.lower()
+
+                if hotword in trigger:
+                    print('COMANDO: ', trigger)
+                    responde('feedback')
+                    executa_comandos(trigger)
+                    break
+
+            except sr.UnknownValueError:
+                print("Google not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results from Google Cloud Speech service; {0}".format(e))
+    return trigger
+
+def main():
+    while True:
+        monitora_audio()
+
+main()
+
+
+
 def callback(recognizer, audio):
     # received audio data, now we'll recognize it using Google Speech Recognition
     try:
@@ -124,15 +154,15 @@ def callback(recognizer, audio):
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
-r = sr.Recognizer()
-m = sr.Microphone()
-with m as source:
-    r.adjust_for_ambient_noise(source)
+#r = sr.Recognizer()
+#m = sr.Microphone()
+#with m as source:
+   # r.adjust_for_ambient_noise(source)
 
-stop_listening = r.listen_in_background(m, callback)
+#stop_listening = r.listen_in_background(m, callback)
 
-for _ in range(50): time.sleep(0.1)
+#for _ in range(50): time.sleep(0.1)
 
 
-while True: time.sleep(0)
+#while True: time.sleep(0)
 
